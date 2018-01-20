61e6870f38fa26ae32a8138ec137d712cedcd131
Luca Paganelli <luca.paga@gmail.com>
2018-01-20 20:16:41 +0000

    Including 1minute sleep to allow time service to update system time and avoid JWT token verification failure on GCP

--- device/pubsub/rc.run-daemon-off-docker-on-pi.sh
+++ device/pubsub/rc.run-daemon-off-docker-on-pi.sh
@@ -7,4 +7,9 @@ export GCP_PUBSUB_SUBSCRIPTION_COMMANDS=gpio_commands_subscription
 export GCP_PUBSUB_TOPIC_STATUS=gpio_status_topic
 export PI_EMULATE_GPIO=False
 
+echo "Sleeping 1 minute ..."
+sleep 60
+
+echo "Now running daemon!"
+
 /home/pi/iot-poc-01/device/pubsub/rc.run-daemon.sh
